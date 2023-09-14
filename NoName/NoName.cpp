f3072a22422974d34638d927fb89b152b55e04d8
ТОП <>
2023-09-14 18:57:21 +0300

    Добавьте файлы проекта.

---
+++ NoName/NoName.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Point {
+	string firstName;
+	string lastName;
+	int age;
+	int phoneNumber;
+};
+int main() {
+	setlocale(LC_ALL, "");
+	Point user;
+	cout << "Введите имя: " << endl;
+	cin >> user.firstName;
+	cout << "Введите фамилию: " << endl;
+	cin >> user.lastName;
+	cout << "Введите возраст: " << endl;
+	cin >> user.age;
+	cout << "Введите номер: " << endl;
+	cin >> user.phoneNumber;
+}
+
